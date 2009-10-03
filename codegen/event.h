fc69169b8da68e92605f75cac3d5e9e92800c611
Friedrich Weber <fred@reichbier.de>
2009-10-03 15:37:31 +0200

    added a first interface file

---
+++ codegen/event.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#define _EVENT_HAVE_STDINT_H
+#define _EVENT_HAVE_UINT64_T
+#include <event.h>
+
---
+++ codegen/generate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+babbisch -i "/usr/include/ev(.*).h" -o event.json event.h
